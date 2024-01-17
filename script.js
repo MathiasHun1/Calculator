66386457376fd4ae0c15c158ff489e75029fa993
MathiasHun <mathias.hun@gmail.com>
2024-01-17 23:15:19 +0100

    working on it

--- script.js
+++ script.js
@@ -7,7 +7,13 @@ const operator = document.querySelector('#operator');
 const operatorsArr = ['*', '/', '+', '-'];
 const result = document.querySelector('#result');
 
-
+let firstNumber;
+let lastNumber;
+let op;
+let firstNumberFlag = false;
+let lastNumberflag = false;
+let opFlag = false;
+let resultFlag = false;
 
 function add (a, b) {
     return a + b;    
@@ -24,3 +30,19 @@ function multiply (a, b) {
 function divide (a, b) {
     return a / b;
 }
+
+function operate(a, b, c, ) {
+    if (b === '+') return add(a, c);
+    if (b === '-') return subtract(a, c);
+    if (b === '*') return multiply(a, c);
+    if (b === '/') return divide(a, c);
+}
+
+document.addEventListener('click', (e) => {
+
+    if (firstNumberFlag === false &&
+        lastNumberflag === false &&
+        opFlag === false) {
+            firstNumber = e.target.outerText;       
+        }
+})
