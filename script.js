d3937cabbb641edbb43aba351154c437520c969a
MathiasHun <mathias.hun@gmail.com>
2024-01-17 22:55:47 +0100

    create a new version because the previous was bad

--- script.js
+++ script.js
@@ -5,65 +5,22 @@ const firstNum = document.querySelector('#first-num');
 const lastNum = document.querySelector('#last-num');
 const operator = document.querySelector('#operator');
 const operatorsArr = ['*', '/', '+', '-'];
-
-let firstNumber = 0;
-let op;
-let lastNumber = 0;
-let firstNumberFlag = false;
-let operatorFlag = false;
-let secondNumberFlag = false;
-
-buttonsCont.addEventListener('click', (e) => {
-    let clickedButton = e.target.outerText
-    if (firstNumberFlag === false) {
-        if ( !isNaN(parseInt(clickedButton)) 
-            && (firstNum.textContent !== '')) {
-            firstNum.textContent = firstNum.textContent + clickedButton;
-            firstNumber = firstNum.textContent;
-        } else if (!isNaN(parseInt(clickedButton)) 
-            && (firstNum.textContent == '')) {
-            firstNum.textContent = clickedButton;
-            firstNumber = firstNum.textContent;
-        }        
-    }
-
-    if (operatorsArr.includes(clickedButton) 
-        && (operator.textContent === '') 
-        && (firstNum.textContent !== '')) {
-            operator.textContent = clickedButton;
-            op = clickedButton;
-            firstNumberFlag = true;
-            operatorFlag = true;
-    }
-
-    if ((firstNumberFlag === true) && (operatorFlag === true)) {
-        if ( !isNaN(parseInt(clickedButton)) 
-            && (lastNum.textContent !== '')) {
-                lastNum.textContent = lastNum.textContent + clickedButton;
-                lastNumber = lastNum.textContent;
-        } else if (!isNaN(parseInt(clickedButton)) 
-            && (lastNum.textContent == '')) {
-                lastNum.textContent = clickedButton;
-                lastNumber = lastNum.textContent;
-        }        
-    }
-    
-    console.log(firstNumber, lastNumber)
-})
+const result = document.querySelector('#result');
 
 
-// function add {
 
-// }
+function add (a, b) {
+    return a + b;    
+}
 
-// function subtract {
+function subtract (a, b) {
+    return a - b;
+}
 
-// }
+function multiply (a, b) {
+    return a * b;
+}
 
-// function multiply {
-
-// }
-
-// function divide {
-
-// }
+function divide (a, b) {
+    return a / b;
+}
